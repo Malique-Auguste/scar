b1d40358c4a9082faeb41c38e728977c674c2c18
Malique <maliqueauguste@gmail.com>
2023-06-27 12:56:47 -0400

    Started make a neat algorithim instead of linear progression

--- neural_network/neural_net.py
+++ neural_network/neural_net.py
@@ -1,36 +1,69 @@
-from numpy.random import default_rng
-import numpy as np
+from neural_support import *
+import random
 import copy
 
 class NeuralNet:
-    rng = default_rng(1)
+    def __init__(self, input_num, output_num) -> None:
 
-    def __init__(self, shape) -> None:
+        self.links = []
+        self.nodes = []
 
-        self.layers = []
-
-        i = 0
-        while i < len(shape):
-            if i > 0:
-                self.layers.append(self.rng.random((shape[i-1], shape[i])))
-            
-            i += 1
+        self.input_num = input_num
+        self.output_num = output_num
+        self.hidden_num = 0
 
         self.score = 0.0
 
     def evolve(self):
         clone = copy.deepcopy(self)
-        for layer in clone.layers:
-            row = 0
-            while row < len(layer):
-                column = 0
-                while column < len(layer[row]):
-                    if NeuralNet.rng.choice([True, False]):
-                        layer[row, column] += NeuralNet.rng.uniform(-0.5, 0.5)
-                    
-                    column += 1
-                row += 1
+        action = random.random()
         
+        'Evolve hidden link'
+        if action < 0.1:
+            nodes = random.choices(clone.nodes, k = 2)
+            if nodes[0].init_time < nodes[1].init_time:
+                clone.links.append(Link(nodes[0].id, nodes[1].id, random.uniform(-2, 2)))
+
+            elif nodes[0].init_time > nodes[1].init_time:
+                clone.links.append(Link(nodes[1].id, nodes[0].id, random.uniform(-2, 2)))
+
+        #Evolve input link
+        elif action < 0.2:
+            node = random.choice(clone.nodes)
+            input_id =  "i" + random.randrange(0, clone.input_num)
+            clone.links.append(Link(input_id, node.id, random.uniform(-2, 2)))
+
+        #Evolve output link
+        elif random.random() < 0.3:
+            node = random.choice(clone.nodes)
+            output_id =  "o" + random.randrange(0, clone.output_num)
+            clone.links.append(Link(node.id, output_id, random.uniform(-2, 2)))
+        
+        #Evolve node
+        elif random.random() < 0.5:
+            old_link = random.choice(clone.links)
+            old_link_index = clone.links.index(old_link)
+            clone.links.remove(old_link)
+
+            new_node = Node(clone.output_num)
+            clone.nodes.append(new_node)
+            clone.output_num += 1
+
+            new_link_1 = Link(old_link.start, new_node.id, old_link.weight)
+            new_link_2 = Link(new_node.id, old_link.end, old_link.weight)
+
+            clone.links.insert(old_link_index, new_link_1)
+            clone.links.insert(old_link_index + 1, new_link_2)
+
+        #Evolve weight
+        else:
+            link = random.choice(clone.links)
+            link.weight += random.uniform(-1, 1)
+            if link.weight > 2:
+                link.weight = 2
+            elif link.weigt < -2:
+                link.weight = -2
+
         return clone
         
     def propogate(self, input):
--- neural_network/neural_support.py
+++ neural_network/neural_support.py
@@ -1,17 +1,19 @@
 from math import tanh
+import datetime
 
 class Link:
-    def __init__(self, start, end):
+    def __init__(self, start, end, weight):
         self.start = start
         self.end = end
+        self.weight = weight
         return self
 
-class Neuron:
-    def __init__(self, id, weight, links) -> None:
+class Node:
+    def __init__(self, id) -> None:
         self.id = id
-        self.weight = weight
-        self.links = links
+        self.input = 0.0
         self.output = 0.0
+        self.init_time = datetime.datetime.now()
         pass
 
     def propogate(self, input):
